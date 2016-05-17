fc511f90b204cd4deb5f503fb74605d87fd619ce
Jonathan Mah <jonathan.mah.7@gmail.com>
2016-05-17 15:58:57 -0700

    initial commit for dj deploy middleware.

---
+++ sample.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var libstats = require('./lib/libstats');
+var app = express();
+
+
+app.use(libstats.initClient(app, {
+  username: 'zelifus',            /* your github username                  */
+  name: 'test',                   /* your app name                         */
+  port: 8080,                     /* your app port number                  */
+  interval: 600000,               /* suggested reporting interval: 10 min) */
+  url: 'http://www.djdeploy.com/stats'
+}));
+
+app.get('/*', function (req, res) {
+  res.send('Hello World');
+});
+
+var server = app.listen(8080, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port);
+
+});
